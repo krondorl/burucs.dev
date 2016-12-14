552b080d04fc8f0b7ef27af05fb096b495134977
Adam Burucs <adam@onlinetrainer.hu>
2016-12-14 18:08:49 +0100

    Add all
    All files for the new 2016 v2 website

---
+++ index.php
@@ -0,0 +1,425 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Adam Burucs</title>
+  <meta name="description" content="Web development and graphic design from Europe. Modern webpages. Visuals with substance." />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    body {
+      background-color: #555;
+      color: #fff;
+      font-family: 'Arial', sans-serif;
+      line-height: 1.5;
+      margin: 0;
+    }
+    a:link {
+      color: #fff;
+    }
+    h1, h2, h3, h4, h5, h6, p, blockquote, figure, ol, ul {
+      margin: 0;
+      padding: 0 0 1rem;
+    }
+    article {
+      margin: 0;
+      padding: 1rem 0 2rem;
+    }
+    ol, ul {
+      margin: 0;
+      padding: 0 0 1rem 1rem;
+    }
+    h1, h2, h3, h4, h5, h6 {
+      font-family: 'Arial Black', sans-serif;
+      padding-top: 1rem;
+    }
+    .h1, .h2, .h3 {
+      text-align: center;
+    }
+    .h1 {
+      font-size: 2rem;
+    }
+    .dib {
+      display: inline-block;
+    }
+    .container {
+      margin: 0 auto;
+      max-width: 960px;
+      padding: 0 1rem;
+    }
+    .intro {
+      background: #1f2833 url('img/burucs-com-intro-bg.jpg') no-repeat 50% 0 / cover;
+      padding: 5rem 0;
+    }
+    .custom-dev {
+      border-bottom: .5rem double #fff;
+      border-top: .5rem double #fff;
+      margin-top: 2rem;
+    }
+    .expert, .h3 {
+      font-family: 'Arial Narrow', sans-serif;
+    }
+    .touch {
+      background: #147b53;
+    }
+    .agile {
+      background: #147b53 url('img/spiral-bg.svg') no-repeat 50% 50% / 200% 200%;
+    }
+    .latest-work {
+      background-color: #4a7293;
+    }
+    .core-values {
+      background-color: #2e3842;
+    }
+    .footer,
+    .about {
+      background: #232323 url('img/spiral-bg.svg') no-repeat 50% 50% / 200% 200%;
+    }
+    .footer {
+      padding: 2rem 0 3rem;
+    }
+    .contact-touch,
+    .contact {
+      background-color: #147b53;
+      border: .2rem dashed #fff;
+      color: #fff;
+      display: inline-block;
+      letter-spacing: .25rem;
+      padding: 1rem;
+      text-decoration: none;
+      text-transform: uppercase;
+    }
+    .contact-touch {
+      background-color: #666;
+    }
+    .mail {
+      font-size: 1.5rem;
+    }
+    .center {
+      text-align: center;
+    }
+    .right {
+      text-align: right;
+    }
+    .left{
+      text-align: left;
+    }
+    .testimonials {
+      background: #505393 url('img/spiral-bg.svg') no-repeat 50% 50% / 200% 200%;
+    }
+    .brands {
+      background: #505393 url('img/spiral-bg.svg') no-repeat 50% 50% / 200% 200%;
+    }
+    .img-medium {
+      max-width: 15rem;
+      min-width: 15rem;
+      max-height: 15rem;
+    }
+    .flex-row {
+      display: block;
+    }
+    .flex-col {
+      flex: 1;
+    }
+    .arrow-top {
+      display: inline-block;
+      height: 2rem;
+      width: 1.5rem;
+    }
+    .social li {
+      display: inline-block;
+    }
+    .social li:after {
+      content: '|';
+      display: inline-block;
+      margin: 0 .5rem;
+    }
+    .social li:last-of-type:after {
+      content: '';
+    }
+    @media (min-width: 540px) {
+      .flex-row {
+        display: flex;
+      }
+      article {
+        padding: 2rem 0 3rem;
+      }
+    }
+  </style>
+</head>
+<body id="top">
+  <article class="intro">
+    <div class="container">
+      <div class="center">
+        <svg class="img-medium" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 172.188 170.66"><defs><style>.cls-1{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:14px;}</style></defs><title>adam-spiral-logo</title><path class="cls-1" d="M6.617,36.8a97.2,97.2,0,0,1,137.462,0h0a77.761,77.761,0,0,1,0,109.97,62.209,62.209,0,0,1-87.976,0,49.767,49.767,0,0,1,0-70.381,39.813,39.813,0,0,1,56.3,0,31.851,31.851,0,0,1,0,45.044,25.481,25.481,0,0,1-36.035,0,20.384,20.384,0,0,1,0-28.827h0a16.308,16.308,0,0,1,23.062,0,13.046,13.046,0,0,1,0,18.45h0" transform="translate(-1.667 -1.333)"/></svg>
+      </div>
+      <h1 class="h1 custom-dev">More clients and sales with better websites</h1>
+      <h2 class="h2 expert">Bespoke websites and graphic design <br>
+      Beautiful, fast and efficient</h2>
+      <p class="center">
+        <a href="#contact" class="contact">Contact now!</a>
+      </p>
+    </div>
+  </article>
+
+  <article class="modern">
+    <div class="container center">
+      <h2 class="h2">Modern and responsive websites</h2>
+      <p><img src="img/rwd.svg" class="img-medium" alt=""></p>
+      <p>Responsive means it works on any device. Built with the latest standards and tech tools available.</p>
+    </div>
+  </article>
+
+  <article class="agile">
+    <div class="container">
+      <h2 class="h2">Agile product development</h2>
+      <p class="center"><img src="img/agile.svg" alt="" class="img-medium"></p>
+      <p class="center">State of the art methodology for the entire development process.</p>
+      <div class="center">
+        <ul class="left dib">
+          <li>flexibility</li>
+          <li>cost control</li>
+          <li>high visibility</li>
+          <li>active involvement</li>
+        </ul>
+      </div>
+    </div>
+  </article>
+
+  <article class="latest-work">
+    <div class="container">
+      <h2 class="h2">Latest works</h2>
+      <div class="flex-row">
+        <div class="flex-col center">
+          <h3>Expackt</h3>
+          <p>frontend development</p>
+        </div>
+        <div class="flex-col center">
+          <h3>Kezklinika</h3>
+          <p>frontend development</p>
+        </div>
+      </div>
+      <div class="flex-row">
+        <div class="flex-col center">
+          <h3>Deepsec</h3>
+          <p>graphic design</p>
+        </div>
+        <div class="flex-col center">
+          <h3>Taltosjogsi</h3>
+          <p>frontend development, graphic design</p>
+        </div>
+      </div>
+    </div>
+  </article>
+  <article class="brands">
+    <div class="container">
+      <h2 class="h2">Brands I worked together</h2>
+      <p class="center"><img src="img/brands.svg" alt="" class="img-medium"></p>
+      <div class="flex-row">
+        <div class="flex-col center">
+          <h3 class="h3">International companies:</h3>
+          <ul class="dib left">
+            <li>Vodafone</li>
+            <li>Opel</li>
+            <li>Bayer</li>
+            <li>Hello Kitty</li>
+            <li>Tchibo</li>
+          </ul>
+        </div>
+        <div class="flex-col center">
+          <h3 class="h3">Hungarian firms:</h3>
+          <ul class="dib left">
+            <li>Index</li>
+            <li>MOL Group</li>
+            <li>TV2</li>
+            <li>NOL</li>
+            <li>Arukereso</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </article>
+  <article class="core-values">
+    <div class="container">
+      <h2 class="h2">Core Values</h2>
+      <div class="flex-row">
+        <div class="flex-col">
+          <h3 class="center">Wholeness</h3>
+          <p class="center"><img src="img/wholeness.svg" alt="" class="img-medium"></p>
+          <p>I'm living my life caring about all four dimensions:</p>
+          <ul>
+            <li>physical</li>
+            <li>mental</li>
+            <li>emotional</li>
+            <li>spiritual</li>
+          </ul>
+        </div>
+        <div class="flex-col">
+          <h3 class="center">Creation</h3>
+          <p class="center"><img src="img/creation.svg" alt="" class="img-medium"></p>
+          <p>Creating new things is a godlike ability and I feel great to be able to form and shape whatever I'd like to see. It is nice to have a rich life without needing to be financially rich.</p>
+        </div>
+      </div>
+      <div class="flex-row">
+        <div class="flex-col">
+          <h3 class="center">Diversity</h3>
+          <p class="center"><img src="img/diversity.svg" alt="" class="img-medium"></p>
+          <p>Working in different disciplines with various people and products gives me a constant energy to live.</p>
+        </div>
+        <div class="flex-col">
+          <h3 class="center">Learning</h3>
+          <p class="center"><img src="img/learning2.svg" alt="" class="img-medium"></p>
+          <p>I believe in life long learning not just technically, but in every part of our lives.</p>
+        </div>
+      </div>
+    </div>
+  </article>
+  <article class="about">
+    <div class="container">
+      <h2 class="h2">About me</h2>
+      <div class="center">
+        <img src="img/adam-2016-januar-1x.jpg" alt="" class="img-medium">
+      </div>
+      <div class="flex-row">
+        <div class="flex-col">
+          <p>I'm a creative craftsman working in multiple fields, such as:</p>
+          <ul>
+            <li>web development</li>
+            <li>graphic design</li>
+            <li>article writing</li>
+          </ul>
+        </div>
+        <div class="flex-col">
+          <p>My hobbies include also creation:</p>
+          <ul>
+            <li>music composing</li>
+            <li>audio mix engineering</li>
+            <li>drawing</li>
+            <li>cooking</li>
+            <li>and sometimes painting</li>
+          </ul>
+        </div>
+      </div>
+      <p>My name is Adam Burucs I live in Europe, Budapest. I have got 18 years experience in creating websites and 10 years designing graphics. I reached the top3 position in the <a href="https://www.seacoastbank.com/" target="_blank">Seacoast Bank</a> webdesign contest in 2009.</p>
+
+      <div class="flex-row">
+        <div class="flex-col">
+          <p>Graphic style focus:</p>
+          <ul>
+            <li>clean, minimal</li>
+            <li>typography</li>
+            <li>grids</li>
+          </ul>
+        </div>
+        <div class="flex-col">
+          <p>Website focus:</p>
+          <ul>
+            <li>responsive</li>
+            <li>usable</li>
+            <li>accessible</li>
+            <li>performance</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </article>
+  <article class="writing">
+    <div class="container">
+      <h2 class="h2">Writing</h2>
+      <p>I started writing articles in 2012.
+      I work for a major education network called Envato Tuts+ for the code, music and webdesign sections.
+      </p>
+      <ul>
+        <li><a href="https://webdesign.tutsplus.com/tutorials/your-first-responsive-email-build-with-foundation-for-emails--cms-26349" target="_blank">Your First Responsive Email Build With Foundation for Emails</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/creating-a-simple-instagram-plugin--wp-30172" target="_blank">Creating a Simple Instagram Plugin</a></li>
+        <li><a href="http://code.tutsplus.com/articles/quick-tip-html-in-image-captions--wp-26813" target="_blank">Quick Tip: HTML in Image Captions</a></li>
+        <li><a href="http://code.tutsplus.com/articles/quick-tip-using-wp_editor--wp-27136" target="_blank">Quick Tip: Using wp_editor</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/making-a-theme-with-bones-cleaning-up--wp-26560" target="_blank">Making a Theme With Bones: Cleaning Up</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/making-a-theme-with-bones-finishing-off--wp-26550" target="_blank">Making a Theme With Bones: Finishing Off</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/making-a-theme-with-bones-getting-started--wp-26545" target="_blank">Making a Theme With Bones: Getting Started</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/random-quote-plugin-with-custom-post-type--wp-26486" target="_blank">Random Quote Plugin With Custom Post Type</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/wordpress-caching-plugins--wp-26445" target="_blank">WordPress Caching Plugins</a></li>
+        <li><a href="http://code.tutsplus.com/articles/quick-tip-using-the-mysteriously-named-_e-_n-and-__-functions--wp-26092" target="_blank">Quick Tip: Using the Mysteriously Named _e, _n and __ Functions</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/plugins-for-monitoring-your-wordpress-sites-uptime--wp-25884" target="_blank">Plugins for Monitoring Your WordPress Site's Uptime</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/writing-custom-queries-in-wordpress--wp-25510" target="_blank">Writing Custom Queries in WordPress</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/creating-a-simple-twitter-plugin-for-wordpress--wp-25222" target="_blank">Creating a Simple Twitter Plugin for WordPress</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/mini-guide-to-contact-form-7--wp-25086" target="_blank">Mini Guide to Contact Form 7</a></li>
+        <li><a href="http://code.tutsplus.com/articles/a-short-guide-to-trackbacks--wp-24879" target="_blank">A Short Guide to Trackbacks</a></li>
+        <li><a href="http://code.tutsplus.com/tutorials/integrating-superfish-menu-into-a-template--wp-24131" target="_blank">Integrating Superfish Menu Into a Template</a></li>
+        <li><a href="http://code.tutsplus.com/articles/creating-a-random-quote-plugin-for-wordpress--wp-23833" target="_blank">Creating a Random Quote Plugin for WordPress</a></li>
+      </ul>
+    </div>
+  </article>
+  <article class="testimonials">
+    <div class="container">
+      <h2 class="h2">Testimonials</h2>
+      <blockquote>
+        <p>"He is enthusiastic and made a good job with the Bizzabo project."<br>
+        —
+        Kristof Bardos,
+        CEO,
+        Digital Natives</p>
+      </blockquote>
+
+      <blockquote>
+        <p>"Adam is a quiet, hard-working and studious developer."<br>
+        —
+        Peter Bacsanyi,
+        lead developer,
+        Index</p>
+      </blockquote>
+
+      <blockquote>
+        <p>"Adam is a skilled developer. He knows what he is doing and will handle your project well."<br>
+        —
+        James Vincent,
+        site owner,
+        DesignDisease</p>
+      </blockquote>
+
+     <blockquote>
+        <p>"He is a competent and friendly guy, dedicated to providing quality work."<br>
+        —
+        Zoltan Nogradi,
+        project manager,
+        Hello Kitty</p>
+     </blockquote>
+     <blockquote>
+        <p>"Adam contributed to the success of Vodafone.hu Global redesign 2007 as a reliable developer and teammate. He is fast and has got good working capacity."<br>
+        —
+        Gina Slegl,
+        graphic designer,
+        SandMark Ltd</p>
+     </blockquote>
+     <blockquote>
+        <p>"He is curious, easy to work with and willing to learn a lot of new things. His knowledge is always up to date."<br>
+        —
+        Endre Somogyi,
+        CTO,
+        Adverticum</p>
+     </blockquote>
+    </div>
+  </article>
+  <article class="touch" id="contact">
+    <div class="container center">
+      <h2 class="h2">Get in touch</h2>
+      <p>Send me the details of your upcoming project!</p>
+      <p class="mail"><strong>adam.burucs @ gmail.com</strong></p>
+      <p>I'm located in <a href="https://goo.gl/AldUH6" target="_blank">Europe, Hungary, Budapest, 1113</a></p>
+    </div>
+  </article>
+  <footer class="footer center">
+    <div class="container">
+        <ul class="social">
+          <li><a href="https://tutsplus.com/authors/adam-burucs" target="_blank">Envato Tuts+</a></li>
+          <li><a href="https://www.github.com/krondorl" class="icon fa-github" target="_blank"><span class="label">Github</span></a></li>
+          <li><a href="https://www.facebook.com/adamfiregateicegate" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+          <li><a href="https://hu.linkedin.com/in/adamburucs" class="icon fa-linkedin" target="_blank"><span class="label">Linkedin</span></a></li>
+          <li><a href="https://www.behance.net/adamburucs" class="icon fa-behance" target="_blank"><span class="label">Behance</span></a></li>
+        </ul>
+      <p>Some icons from flaticon.com.</p>
+      <p>Hand coded with love in Budapest, 2016.</p>
+      <a href="#top"><img class="arrow-top" src="img/arrow-top.svg" alt=""> Jump to top</a>
+    </div>
+  </footer>
+</body>
+</html>
